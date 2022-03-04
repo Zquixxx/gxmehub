79b03d3d0325d0a605e497292d1b332ca0f95ad8
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2022-03-04 23:47:05 +0000

    local linking

--- gxmes.js
+++ gxmes.js
@@ -3,7 +3,7 @@ try {
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 $$("h2").forEach(el => el.addEventListener("click",function() {
-  location.href = "../play/" + this.textContent.toLowerCase().replace(/\s/g, '').replace(/\:/g, '').replace(/\./g, '') + ".html"
+  location.href = "../play/?" + this.textContent
 }))
 } catch(err) {
   alert(err);
