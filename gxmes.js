45365e5292936f8482e7b66ff44a8cc66ad65512
legends <rayanasad1974@gmail.com>
2022-03-04 09:17:19 -0600

    Gxme Hub Files v3

---
+++ gxmes.js
@@ -0,0 +1,11 @@
+const elemsrendered = () => {
+try {
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+$$("h2").forEach(el => el.addEventListener("click",function() {
+  location.href = "/play/" + this.textContent.toLowerCase().replace(/\s/g, '').replace(/\:/g, '').replace(/\./g, '') + ".html"
+}))
+} catch(err) {
+  alert(err);
+}
+}
